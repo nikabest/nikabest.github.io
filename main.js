14c0623124ec12b9ba7c80e55bf38c77061e7c48
nikabest <84178913+nikabest@users.noreply.github.com>
2022-01-29 17:21:04 +0300

    Update main.js
    Причесал немного год на JS

--- main.js
+++ main.js
@@ -1,69 +1,64 @@
 //Переменные
-let level = 1
-let score = 0
-let result = 0
+let level   = 1
+let score   = 0
+let result  = 0
 
 const scoreToNextLevel = 5
 
 //Элементы 
-const leveltitle = document.querySelector('#leveltitle');
-const problem = document.querySelector('#problem');
-const answers = document.querySelector('#answers');
-const points = document.querySelector('#points');
-const start = document.querySelector('#start');
+const leveltitle    = document.querySelector('#leveltitle');
+const problem       = document.querySelector('#problem');
+const answers       = document.querySelector('#answers');
+const points        = document.querySelector('#points');
+const start         = document.querySelector('#start');
 
 clearPage();
 showQuestion();
 showScore();
 
-
 function clearPage(){
-	leveltitle.innerHTML = ''; 
-    problem.innerHTML = '';
-    answers.innerHTML = '';
-    points.innerHTML = '';
-    start.innerHTML = '';
+	leveltitle.innerHTML    = ''; 
+    problem.innerHTML       = '';
+    answers.innerHTML       = '';
+    points.innerHTML        = '';
+    start.innerHTML         = '';
 }
 
 
 function showQuestion(){
 	
-    //Загаловок
+    //Заголовок
 	const headerTemplate = `<h2 id="leveltitle">%title%</h2>`;
     leveltitle.innerHTML = headerTemplate.replace('%title%', "Уровень " + level);
 
     //В зависимости от уровня выводим зарные примеры
-    let firstNumber = 0;
-    let secondNumber = 0;
-    let problemtitle = "";
+    let firstNumber     = 0;
+    let secondNumber    = 0;
+    let problemtitle    = "";
 
     if (level === 1) {
-        firstNumber = getRandomInt(1, 99);
-        secondNumber = getRandomInt(1, 99);
-        result = firstNumber + secondNumber;
-        problemtitle = firstNumber + " + " + secondNumber + " = ...";
+        firstNumber     = getRandomInt(1, 99);
+        secondNumber    = getRandomInt(1, 99);
+        result          = firstNumber + secondNumber;
+        problemtitle    = firstNumber + " + " + secondNumber + " = ...";
     } else {
-        firstNumber = getRandomInt(1, 999);
-        secondNumber = getRandomInt(1, 999);
+        firstNumber     = getRandomInt(1, 999);
+        secondNumber    = getRandomInt(1, 999);
         if (firstNumber > secondNumber) {
-            result = firstNumber - secondNumber;
-            problemtitle = secondNumber + " + ... = " + firstNumber;
+            result          = firstNumber - secondNumber;
+            problemtitle    = secondNumber + " + ... = " + firstNumber;
         } else {
-            result = secondNumber - firstNumber;
-            problemtitle = firstNumber + " + ... = " + secondNumber;
+            result          = secondNumber - firstNumber;
+            problemtitle    = firstNumber + " + ... = " + secondNumber;
         }
-        console.log(result) 
     }
-    const problemTemplate = `<p id="problem">%problemtitle%</p>`;
-    problem.innerHTML =  problemTemplate.replace('%problemtitle%', problemtitle);
-
-
+    const problemTemplate   = `<p id="problem">%problemtitle%</p>`;
+    problem.innerHTML       =  problemTemplate.replace('%problemtitle%', problemtitle);
 
     //Выводим кнопки с ответами, три штуки одна рандомная верная
-
-    const numberOfGoodAnswer = getRandomInt(1, 3);
-    const butTemplate = `<input class="btn1" type="button" value="%value%" id="button%i%">`;
-    let buttons = "";
+    const numberOfGoodAnswer    = getRandomInt(1, 3);
+    const butTemplate           = `<input class="btn1" type="button" value="%value%" id="button%i%">`;
+    let buttons                 = "";
 
     for (let i = 1; i < 4; i++){
 
@@ -102,10 +97,10 @@ function showQuestion(){
     } else {
         button3.onclick = badAnswer;
     }
-
 }
 
 
+//Функция возвращает случайте число из переданного диапазона
 function getRandomInt(min, max) {
     let rand = min - 0.5 + Math.random() * (max - min + 1);
     return Math.round(rand);
@@ -123,8 +118,8 @@ function goodAnswer(){
             //Победа
             level = 0;
             clearPage();
-            problem.innerHTML =  `<p id="problem">Вы победили!</p>`;
-            start.innerHTML = `<a class="btn" href="level.html">Заново</a>`;       
+            problem.innerHTML   = `<p id="problem">Вы победили!</p>`;
+            start.innerHTML     = `<a class="btn" href="level.html">Заново</a>`;       
         }
     }
     
@@ -141,6 +136,6 @@ function badAnswer(){
 
 
 function showScore(){
-    const pointsTemplate = `<p>%score%/%scoreToNextLevel%</p>`;
-    points.innerHTML = pointsTemplate.replace('%score%', score).replace('%scoreToNextLevel%', scoreToNextLevel); 
+    const pointsTemplate    = `<p>%score%/%scoreToNextLevel%</p>`;
+    points.innerHTML        = pointsTemplate.replace('%score%', score).replace('%scoreToNextLevel%', scoreToNextLevel); 
 }
